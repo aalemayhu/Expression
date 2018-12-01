daecdba488046ea10668bf41894cd2d9ec9cd366
Alexander Alemayhu <alexander@alemayhu.com>
2018-12-01 19:09:49 +0100

    Use NSExpression

--- Sources/Expression/Expression.swift
+++ Sources/Expression/Expression.swift
@@ -1,7 +1,18 @@
+import Foundation
+
 struct Expression {
     let value: String
 
     public func evaluate() -> String {
-        return "4"
+        /**
+         * XXX: This won't work on Linux. Someone needs to implement NSExpression upstream[0], maybe you can do it?
+         * [0]: https://github.com/apple/swift-corelibs-foundation/blob/master/Foundation/NSExpression.swift
+         */
+        let exp = NSExpression(format: value)
+        if let result = exp.expressionValue(with: nil, context: nil) {
+            return "\(result)"
+        }
+
+        return value
     }
 }
--- Tests/ExpressionTests/ExpressionTests.swift
+++ Tests/ExpressionTests/ExpressionTests.swift
@@ -2,14 +2,28 @@ import XCTest
 @testable import Expression
 
 final class ExpressionTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Expression().text, "Hello, World!")
+
+    private func check(input: String, expected: String) {
+        let actual = Expression(value: input)
+        XCTAssertEqual(expected, actual.evaluate())
     }
 
+    func testSimpleAddition() {
+        self.check(input: "2 + 2", expected: "4")
+        self.check(input: "1 + 1", expected: "2")
+    }
+
+    // TODO: test subtraction
+    // TODO: test division
+    // TODO: test long division
+    // TODO: test floating point values
+    // TODO: test multiplication
+    // TODO: test PEMDAS
+    // TODO: test exponents
+    // TODO: test paranthesis
+
+
     static var allTests = [
-        ("testExample", testExample),
+        ("testSimpleAddition", testSimpleAddition),
     ]
 }
